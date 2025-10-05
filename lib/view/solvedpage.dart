02434852fb6ffcec6ed83224ee99333e8ed9916d
shreeyansh64 <shreeyansh64@gmail.com>
2025-10-05 17:32:27 +0530

    Updated SolvedPage

--- lib/view/solvedpage.dart
+++ lib/view/solvedpage.dart
@@ -1,5 +1,6 @@
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
+import 'package:leet/view/chart.dart';
 
 class SolvedPage extends StatefulWidget {
   const SolvedPage({super.key});
@@ -26,25 +27,7 @@ class _SolvedPageState extends State<SolvedPage> {
           ),
         ),
       ),
-      body: Stack(
-        alignment: Alignment.center,
-        children: [
-          Text("Questions Solved",style: TextStyle(color: Colors.black,fontWeight: FontWeight.w400,fontSize: 30),),
-          PieChart(
-            swapAnimationDuration: Duration(milliseconds: 1000),
-            swapAnimationCurve: Curves.easeInOut,
-            
-            PieChartData(
-              sections: [
-                PieChartSectionData(value: 20, color: Colors.red[300]),
-                PieChartSectionData(value: 10, color: Colors.blueGrey[400]),
-                PieChartSectionData(value: 40, color: Colors.blue[400]),
-                PieChartSectionData(value: 30, color: Colors.greenAccent[400]),
-              ],
-            ),
-          ),
-        ],
-      ),
+      body: Chart(), 
     );
   }
 }
