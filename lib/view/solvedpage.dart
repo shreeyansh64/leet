187bf4b306958e0eee50a6f04d3e5a9b8c482cd1
shreeyansh64 <shreeyansh64@gmail.com>
2025-10-05 16:50:25 +0530

    Added Solved Page

--- lib/view/solvedpage.dart
+++ lib/view/solvedpage.dart
@@ -1,3 +1,4 @@
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 
 class SolvedPage extends StatefulWidget {
@@ -24,6 +25,25 @@ class _SolvedPageState extends State<SolvedPage> {
             ),
           ),
         ),
+      ),
+      body: Stack(
+        alignment: Alignment.center,
+        children: [
+          Text("Questions Solved",style: TextStyle(color: Colors.black,fontWeight: FontWeight.w400,fontSize: 30),),
+          PieChart(
+            swapAnimationDuration: Duration(milliseconds: 1000),
+            swapAnimationCurve: Curves.easeInOut,
+            
+            PieChartData(
+              sections: [
+                PieChartSectionData(value: 20, color: Colors.red[300]),
+                PieChartSectionData(value: 10, color: Colors.blueGrey[400]),
+                PieChartSectionData(value: 40, color: Colors.blue[400]),
+                PieChartSectionData(value: 30, color: Colors.greenAccent[400]),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
